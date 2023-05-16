f822e886811f29b6f5d2f9bf22472ea9e90f5864
Guilherme Bandeira <107276182+Asphyan@users.noreply.github.com>
2023-05-16 03:44:40 -0300

    Create Lista2N1_Q20.py

---
+++ Lista02_N1_Questões/Lista2N1_Q20.py
@@ -0,0 +1,7 @@
+'''
+Crie um grafo vazio.
+Peça para o usuário digitar os vértices e as arestas desse grafo.
+O usuário deve informar os pares de vértices que formam cada aresta.
+Em seguida, mostre o grafo.
+'''
+
